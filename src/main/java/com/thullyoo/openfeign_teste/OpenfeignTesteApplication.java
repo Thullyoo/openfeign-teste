5ec682676aabe7996464e13e1d9248036b1448a2
Thullyoo <thullyofilho@gmail.com>
2024-10-28 15:08:28 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/openfeign_teste/OpenfeignTesteApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.openfeign_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfeignTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfeignTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/openfeign_teste/OpenfeignTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.openfeign_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
